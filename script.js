e267d7a7c4e82ceedf51b486023f4fc37f32a0b2
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-15 14:03:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-//complete this code
-class Person {}
-
-class Student extends Person {}
-
-class Teacher extends Person {}
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Person {
+    constructor(name, age) {
+      this._name = name;
+      this._age = age;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get age() {
+      return this._age;
+    }
+  
+    set age(value) {
+      this._age = value;
+    }
+  }
+  
+  class Student extends Person {
+    study() {
+      console.log(`${this.name} is studying`);
+    }
+  }
+  
+  class Teacher extends Person {
+    teach() {
+      console.log(`${this.name} is teaching`);
+    }
+  }
+  
+  // Attach classes to the window object
+  window.Person = Person;
+  window.Student = Student;
+  window.Teacher = Teacher;
+  
